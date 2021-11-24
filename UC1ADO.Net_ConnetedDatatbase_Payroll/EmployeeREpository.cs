47882048aa02540682ea02a02f5c190fb2ec19e7
Vishal@7038 <vishalrathod7038@gmail.com>
2021-11-24 23:47:43 +0530

    [vishal] Connected to databse Payroll service

---
+++ UC1ADO.Net_ConnetedDatatbase_Payroll/EmployeeREpository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC1ADO.Net_ConnetedDatatbase_Payroll
+{
+     public class EmployeeREpository
+    {
+
+        //uc 1 ADO.NET Connection databse
+        public static string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Employee_payroll;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        // createdconnection objto connect with database
+        SqlConnection sqlConnection = new SqlConnection(connectionString);
+    }
+}
---
+++ UC1ADO.Net_ConnetedDatatbase_Payroll/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UC1ADO.Net_ConnetedDatatbase_Payroll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
